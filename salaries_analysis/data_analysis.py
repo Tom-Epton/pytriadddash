be923abfa33c1a8ff4a007125ff5af6346b6d0b7
Tom-Epton <65612739+Tom-Epton@users.noreply.github.com>
2024-11-13 11:53:20 +0000

    Update data_analysis.py

--- salaries_analysis/data_analysis.py
+++ salaries_analysis/data_analysis.py
@@ -9,10 +9,10 @@ Created on Thu Nov  7 11:27:46 2024
 import pandas as pd
 
 # Load the CSV file
-file_path = 'salaries_DS.csv'  # Replace with your actual file path
+file_path = 'salaries_DS.csv'  
 data = pd.read_csv(file_path)
 
-# Display basic information
+# display basic info
 print("First 5 rows of the dataset:")
 print(data.head())
 
